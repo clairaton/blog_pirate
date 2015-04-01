eabb3a0a930e41a5308fb528ce7dc838b9f97b6a
clairaton <clairaton@gmail.com>
2015-04-01 08:45:25 +0200

    prog

--- program.php
+++ program.php
@@ -2,8 +2,42 @@
 include 'header.php';
 ?>
 <div class="prog col-xs-12">
-	<h2>Mille sabords, le programme n'est pas disponible!!!</h2>
-	<img src="img/bulle-bd-insultes.png" alt="jurons bd">
+	<h2>Programme de la semaine</h2>
+	<aside class="hook hidden-xs hidden-sm col-md-4">
+	</aside>
+	<div id="planning" class="col-xs-12 col-md-8">
+		<h3 class="col-xs-3">Lundi</h3>
+		<ul class="col-xs-9">
+			<li>Voyage Aller</li>
+			<li>Pic Nique</li>
+			<li>Découverte des Chambres</li>
+			<li>Jeux collectifs</li>
+		</ul>
+		<hr>
+		<h3 class="col-xs-3">Mardi</h3>
+		<ul class="col-xs-9">
+			<li>Visite guidée de l'île Tristan, son phare et son fort</li>
+		</ul>
+		<hr>
+		<h3 class="col-xs-3">Mercredi</h3>
+		<ul class="col-xs-9">
+			<li>Ballade sur le port: musée, visite des bateaux et exposition sur le monde marin</li>
+		</ul>
+		<hr>
+		<h3 class="col-xs-3">Jeudi</h3>
+		<ul class="col-xs-9">
+			<li>Chasse au trésor</li>
+			<li>Boum</li>
+		</ul>
+		<hr>
+		<h3 class="col-xs-3">Vendredi</h3>
+		<ul class="col-xs-9">
+			<li>Rangement des chambres, inventaire</li>
+			<li>Jeux collectifs</li>
+			<li>Voyage retour</li>
+			<li>Arrivée à la gare à 20h56</li>
+		</ul>
+	</div>
 </div>
 <?php
 include 'footer.php';
